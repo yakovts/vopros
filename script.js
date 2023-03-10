b2d896ee769f859a88e67a3c3e3e967fb349a772
yakovts <127451819+yakovts@users.noreply.github.com>
2023-03-10 16:29:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+language="JavaScript"
+function expandit(id){
+    obj = document.getElementById(id);
+    if (obj.style.display=="none") obj.style.display="";
+    else obj.style.display="none";
+}
